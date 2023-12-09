1f801939f261d59dcad7fecb6ca00b6a4e888914
Lukasz Szypulewski <szyp86@gmail.com>
2023-12-09 15:00:23 +0100

    add side filter genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Button,
+  CircularProgress,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material"
+
+import useGenres from "../hooks/useGenres"
+import getCroppedImageUrl from "../services/image-url"
+import useGamesQueryStore from "../store"
+
+const GenreList = () => {
+  const { data, isLoading, error } = useGenres()
+  const selectedGenreId = useGamesQueryStore((s) => s.gameQuery.genreId)
+  const setSelectedGenreId = useGamesQueryStore((s) => s.setGenreId)
+  if (error) return null
+
+  if (isLoading) return <CircularProgress />
+
+  return (
+    <>
+      <Typography
+        variant="h5"
+        marginTop={9}
+        marginBottom={3}
+        textAlign="center"
+      >
+        Genres
+      </Typography>
+      <List>
+        {data?.results.map((genre) => (
+          <ListItem key={genre.id}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="flex-start"
+              gap={1}
+              width="100%"
+            >
+              <img
+                height="32px"
+                width="32px"
+                style={{ borderRadius: "8px" }}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                fullWidth
+                style={{
+                  fontWeight: genre.id === selectedGenreId ? "bold" : "normal",
+                }}
+                onClick={() => setSelectedGenreId(genre.id)}
+              >
+                {genre.name}
+              </Button>
+            </Box>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  )
+}
+
+export default GenreList
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,11 +3,14 @@ import Grid from "@mui/material/Unstable_Grid2"
 
 import GameHeading from "../components/GameHeading"
 import GameGrid from "../components/GameGrid"
+import GenreList from "../components/GenreList"
 
 const HomePage = () => {
   return (
     <Grid container>
-      <Grid xl={2}>Genres</Grid>
+      <Grid xl={1.5}>
+        <GenreList />
+      </Grid>
       <Grid xs={10}>
         <GameHeading />
         <Box display="flex" my={2}>
