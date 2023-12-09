64245c23c39746b7f9a5394a994799aecf5a2693
Lukasz Szypulewski <szyp86@gmail.com>
2023-12-09 13:06:45 +0100

    build game detail page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,17 @@ import { createBrowserRouter } from "react-router-dom"
 import ErrorPage from "./pages/ErrorPage"
 import Layout from "./pages/Layout"
 import HomePage from "./pages/HomePage"
+import GameDetailPage from "./pages/GameDetailPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "games/:slug", element: <GameDetailPage /> },
+    ],
   },
 ])
 
